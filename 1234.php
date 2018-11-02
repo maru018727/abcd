9d5516b8e65f42ef9bc67856612d4a0106fa66a7
maru018727 <40679169+maru018727@users.noreply.github.com>
2018-11-02 23:34:23 +0800

    Update 1234.php

--- 1234.php
+++ 1234.php
@@ -1,1 +1,10 @@
-echo json_encode("123");
+$questions=array(
+            1=>array(
+                "question"=>"<<惡魔教室>>影片中主角Rainer Wenger創立了什麼組織？",
+                "answerA"=>"浪潮",
+                "answerB"=>"退潮",
+                "answerC"=>"漲潮",
+                "answerD"=>"潮",
+                "Correct"=>"answerA")
+                );
+echo json_encode($question);
